c0d19329b55a12da43e68c6b3bd160c1dbef5ed2
ken-nah <Kennyadduz@gmail.com>
2019-05-30 22:22:28 +0300

    use effect & use state

--- src/App.js
+++ src/App.js
@@ -1,9 +1,38 @@
-import React from 'react';
+import React, { useState } from "react";
+
+import Joke from './Joke'
 
 function App() {
+  const [userQuery, setUserQuery] = useState("");
+
+  const updateSearchQuery = event => {
+    setUserQuery(event.target.value);
+  };
+
+  const searchQuery = () => {
+    window.open(
+      `https://google.com/search?query=${userQuery}`,
+      "_blank"
+    );
+  };
+
+  const handleKeyPress = event => {
+    if (event.key === "Enter") searchQuery();
+  };
+
   return (
     <div className="App">
-      Header
+      <h1>Hello Kennah!</h1>
+      <div className="form">
+        <input
+          type="text"
+          onChange={updateSearchQuery}
+          onKeyDown={handleKeyPress}
+        />
+        <button onClick={searchQuery}>Search</button>
+      </div>
+      <hr/>
+      <Joke />
     </div>
   );
 }
