35708a62a011c46158c64d6f9b9eace034fdd003
ken-nah <kennyadduz@gmail.com>
2019-06-13 11:19:12 +0300

    tasks component - part 2

--- src/Task.js
+++ src/Task.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import uuid from "uuidv4";
 
-const Task = () => {
+function Task() {
   const [taskText, setTaskText] = useState("");
   const [tasks, setTasks] = useState([]);
   const [completedTasks, setCompletedTasks] = useState([]);
@@ -14,6 +14,18 @@ const Task = () => {
     setTasks([...tasks, { id: uuid(), taskText }]);
   };
 
+  const completeTask = completedTask => () => {
+    setCompletedTasks([...completedTasks, completedTask]);
+    setTasks(tasks.filter(task => task.id !== completedTask.id));
+  };
+
+  const deleteTask = task => () => {
+    setCompletedTasks(completedTasks.filter(t => t.id !== task.id));
+  };
+
+  console.log("tasks", tasks);
+  console.log("completedTasks", completedTasks);
+
   return (
     <div>
       <div className="form">
@@ -23,11 +35,29 @@ const Task = () => {
       <div className="task-list">
         {tasks.map(task => {
           const { id, taskText } = task;
-          return <div key={id}>{taskText}</div>;
+          return (
+            <div key={id} onClick={completeTask(task)}>
+              {taskText}
+            </div>
+          );
+        })}
+      </div>
+      <div className="completed-list">
+        {completedTasks.map(task => {
+          const { id, taskText } = task;
+          return (
+            <div key={id}>
+              {taskText}{" "}
+              <span className="delete-task" onClick={deleteTask(task)}>
+                {" "}
+                x
+              </span>
+            </div>
+          );
         })}
       </div>
     </div>
   );
-};
+}
 
 export default Task;
